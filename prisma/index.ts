dea67e5c45db24a0782aafd3eeee39f7c8458955
Olabosinde Oladipo <olabosindeoladipo@gmail.com>
2023-04-02 15:25:20 +0100

    Code cleanup

--- prisma/index.ts
+++ prisma/index.ts
@@ -1,20 +1,3 @@
-// import { PrismaClient } from "@prisma/client";
-// import { DeepMockProxy, mockDeep, mockReset } from "jest-mock-extended";
-
-// const isTestEnvironment = process.env.NODE_ENV === "test";
-
-// let prismaInstance: PrismaClient | undefined;
-// let prismaMockInstance: DeepMockProxy<PrismaClient>;
-
-// prismaMockInstance =
-//   mockDeep<PrismaClient>() as unknown as DeepMockProxy<PrismaClient>;
-
-// export const prisma = (() => {
-//   return isTestEnvironment
-//     ? prismaMockInstance
-//     : prismaInstance || new PrismaClient();
-// })();
-
 import { PrismaClient } from "@prisma/client";
 import { DeepMockProxy, mockDeep } from "jest-mock-extended";
 
